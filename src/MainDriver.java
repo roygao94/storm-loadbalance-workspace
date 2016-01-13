d25d083c5c3797c9604745a22e8d7a0be8a21e02
Roy Gao <roy_gaoz@hotmail.com>
2016-01-13 17:33:39 +0800

    创建Controller、后续主类MainDriver

---
+++ src/MainDriver.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Roy Gao on 1/13/2016.
+ */
+public class MainDriver {
+
+	// strom jar MainDriver.jar MainDriver
+	// [name] load-balance  [local/remote] remote   ...
+	public static void main(String[] args) {
+
+	}
+}
--- src/bolt/Controller.java
+++ src/bolt/Controller.java
@@ -4,70 +4,58 @@ import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.IRichBolt;
 import backtype.storm.topology.OutputFieldsDeclarer;
-import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
-import backtype.storm.tuple.Values;
 import io.Parameters;
 import redis.clients.jedis.Jedis;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by Roy Gao on 1/9/2016.
+ * Created by Roy Gao on 1/13/2016.
  */
-public class UBolt implements IRichBolt {
+public class Controller implements IRichBolt {
 
 	TopologyContext context;
 	OutputCollector _collector;
-	String ID;
 	int DBoltNumber;
 
 	private String host;
 	private int port;
 	private transient Jedis jedis;
 
-	Map<Integer, Integer> routingTable;
-
-	public UBolt(String host, int port) {
+	public Controller(String host, int port) {
 		this.host = host;
 		this.port = port;
 	}
 
 	@Override
-	public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
-		routingTable = new HashMap<>();
+	public void prepare(Map map, TopologyContext context, OutputCollector collector) {
 		this.context = context;
 		_collector = collector;
-		ID = context.getThisComponentId();
 		DBoltNumber = context.getComponentTasks("dbolt").size();
 	}
 
 	@Override
 	public void execute(Tuple tuple) {
 		Jedis jedis = getConnectedJedis();
-		if (jedis.exists(Parameters.REDIS_RT + ID)) {
-			// update routing table
+		try {
+			Thread.sleep(10000);
 
+			for (int i = 0; i < DBoltNumber; ++i)
+				jedis.lpush(Parameters.REDIS_SUM + context.getComponentTasks("dbolt").get(i));
+
+		} catch (Exception e) {
 		}
-
-		String line = tuple.getString(0);
-		String[] split = line.split(",");
-		int key = Integer.parseInt(split[0]);
-		int g = Integer.parseInt(split[1]);
-		int taskID = context.getComponentTasks("dbolt").get(routingTable.containsKey(key) ? routingTable.get(key) : key % DBoltNumber);
-
-		_collector.emitDirect(taskID, tuple, new Values(key, g));
-		_collector.ack(tuple);
 	}
 
 	@Override
 	public void cleanup() {
+
 	}
 
 	@Override
-	public void declareOutputFields(OutputFieldsDeclarer declarer) {
-		declarer.declare(new Fields("key", "g"));
+	public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+
 	}
 
 	@Override
